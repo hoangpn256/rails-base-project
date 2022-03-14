96fa4f89b025f34ae78209ee7071aa157cc65a05
HoangPN <pnhoang.bk@gmail.com>
2022-03-14 21:55:14 +0700

    feat: add devise views by bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@
 
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords',
+    confirmations: 'users/confirmations',
   }
 
   root to: 'home#index'
